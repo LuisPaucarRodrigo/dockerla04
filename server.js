a8083f9e3f3d30d560d90ca9b1f3279aa698aae7
luis <luis.paucar.ro@tecsup.edu.pe>
2023-04-11 11:37:09 -0500

    lab04

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ const express = require('express');
 
 //Create an app
 const app = express();
+app.get('/clientes', (req, res) => {
+  res.send('Esta es la ruta de clientes de la tienda de luis');
+});
+
+// Route for products
+app.get('/productos', (req, res) => {
+  res.send('Esta es la ruta de productos de paucar');
+});
+
+// Default route
 app.get('/', (req, res) => {
-    res.send('Luis Paucar Rodrigo\n');
+  res.send('Luis paucar\n');
 });
 
 //Listen port
-const PORT = 8080;
+const PORT = 9000;
 app.listen(PORT);
 console.log(`Running on port ${PORT}`);
